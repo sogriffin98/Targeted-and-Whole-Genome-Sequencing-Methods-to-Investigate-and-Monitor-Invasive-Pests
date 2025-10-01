784f0b5fd9b5186f539182fd52616508e4287c5b
Sarah Griffin <117275745+sogriffin98@users.noreply.github.com>
2025-10-01 11:04:36 +0100

    Create HashSeqs.pl

---
+++ Data-Chapter-1/MiSeq-Data-Preparation/HashSeqs.pl
@@ -0,0 +1,29 @@
+#!/usr/bin/perl
+
+$file = $ARGV[0];
+$lib = $ARGV[1];
+
+open(FILE, "<$file")
+	or die;
+
+while (<FILE>) {
+		
+	$seq_line = <FILE>;
+	chomp($seq_line);
+	$tags{$seq_line}++;
+
+	$_ = <FILE>;
+	$_ = <FILE>;
+}
+close FILE;
+
+
+$z = 1;
+foreach $key (sort { $tags{$b} <=> $tags{$a} } keys %tags) {
+
+	print ">$lib;$z;$tags{$key}\n";
+	print "$key\n";
+
+	$z++;
+}
+
